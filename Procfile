978f98dafa198ceb7c037ccd0dcc27dcb4c32e55
kzmaro <maronkazu94@gmail.com>
2019-02-25 12:17:10 +0900

    protocol add

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: guicorn app:app
+web: gunicorn -b 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def result():
       return render_template("form.html",st=st)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
